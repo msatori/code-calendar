3c52789c24dc3ddb04433ed68970b15bff61a91c
Maya Santiago <69778090+msatori@users.noreply.github.com>
2020-10-02 19:32:14 -0500

    added HTML time blocks

---
+++ assets/js/script.js
@@ -0,0 +1,15 @@
+//variables
+var tasks = {};
+
+
+//create schedule
+    //first make the layout of the tasks-standard business hours
+    //add functionality for clicking on event and inputting text
+    //save text in local storage
+
+
+//display current date
+    //use moments.js for date and time 
+
+
+//save JSON
