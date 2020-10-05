23d399194f2d5c628d55d99ad2d0c4879a7a5576
Maya Santiago <69778090+msatori@users.noreply.github.com>
2020-10-04 23:51:01 -0500

    prevent default not working

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,75 +3,59 @@
 var currentDayEl = document.getElementById("currentDay");
 var saveButonEl = document.getElementById("button");
 
-$(document).ready(function() {
-const times = ["9", "10", "11", "12", "13", "14", "15", "16", "17"];
-const arrayItems = [];
+$(document).ready(function (event) {
+    const times = ["9", "10", "11", "12", "13", "14", "15", "16", "17"];
+    const arrayItems = [];
 
-//create a function to display current time
+    //create a function to display current time
 
- //display current day-make function
- var displayDate = moment().format('MMMM Do YYYY');
- currentDayEl.textContent = displayDate;
- console.log(displayDate);
+    //display current day-make function
+    var displayDate = moment().format('MMMM Do YYYY');
+    currentDayEl.textContent = displayDate;
+    console.log(displayDate);
 
-
-//save description info to local storage 
-
-  
     //empty array to house time
-    $(".saveBtn").on("click", function () {
+    $(".saveBtn").on("click", function (e) {
+        e.preventDefault();
         //save text input to local storage
         var time = $(this).parent().attr("id");
         var text = $(this).siblings(".description").val();
-        
+
         localStorage.setItem(text, time);
-        
-        arrayTime.push(time);
+        localStorage.setItem(text, JSON.stringify(text));
+
+        arrayItems.push(time);
         console.log(arrayItems);
+
+
     });
 
- 
+    //loop through times to set color
+    function updateTime() {
+        var currentTime = moment().hours();
+        console.log(currentTime);
+        $(".description").each(function () {
+            var timeSlot = i = 0; times.length; i++
+            if (timeSlot < currentTime) {
+                $(this).addClass("past");
+                $(this).removeClass("present", "future");
+            } else if (timeSlot === currentTime) {
+                $(this).addClass("present");
+                $(this).removeClass("past", "future");
+            } else {
+                $(this).addClass("present");
+                $(this).removeClass("past", present);
+            }
+        });
 
-   
-updateTime();
+    };
+    updateTime();
+    //check to see if time needs to be update by checking time left until next hour
+    var timeCheck = setInterval(updateTime, 15000);
+    console.log(timeCheck);
 
-function updateTime() {
-    var currentTime = moment().hours(); 
-    $(".description").each(function(){
-        var timeSlot =  i = 0; times.length; i++
-        if (timeSlot < currentTime) {
-            $(this).addClass("past");
-            $(this).removeClass ("present", "future");
-        } else if (timeSlot === currentTime) {
-            $(this).addClass("present");
-            $(this).removeClass("past", "future");
-        }else {
-            $(this).addClass("present");
-            $(this).removeClass("past", present);
-        }
-    });
 
-}
+
 });
-//call items out of local storage
 
-
-
-
-
-
-     
-    
-
-
-    
-    
-
-
-
-
-
-
-
-//save text in local storage--use seperate functions for each hour??
 
